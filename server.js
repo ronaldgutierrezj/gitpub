6d4ec4994d4f5921773c325723dd47651966bcb7
ronald <ronaldgutierrezj@gmail.com>
2021-09-14 20:42:02 -0500

    third one

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const drinks = require("./models/drinks")
 
 // Route: Match http://localhost:3000/"
 app.get('/', (req, res) => {
   res.send('Welcome to the Gitpub App!');
 });
 
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+  });
 // Web server. Listen on port 3000
 
 app.listen(port, ()=>{
