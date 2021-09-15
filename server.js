53ce1bddb10e064955a084d96dbca951160df3aa
ronald <ronaldgutierrezj@gmail.com>
2021-09-14 20:31:49 -0500

    second one

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 // Route: Match http://localhost:3000/"
 app.get('/', (req, res) => {
-  console.log("HTTP GET / received");
-  res.send('Hello SEI!');
+  res.send('Welcome to the Gitpub App!');
 });
 
 // Web server. Listen on port 3000
-app.listen(3000, ()=>{
+
+app.listen(port, ()=>{
     console.log("Express is listening on port 3000...")
   });
 
